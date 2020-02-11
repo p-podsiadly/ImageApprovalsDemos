3a61c318c626aa31ea21059cfbb05777a216f620
Piotr Podsiadly <ppodsiadly@mykolab.com>
2020-02-11 10:24:15 +0100

    e Updated ImageApprovals

--- gl_demo/src/main.cpp
+++ gl_demo/src/main.cpp
@@ -8,6 +8,4 @@ using namespace ApprovalTests;
 auto dirDisposer = Approvals::useApprovalsSubdirectory("../approval_tests");
 
 auto comparatorDisposer
-    = FileApprover::registerComparatorForExtension(
-        ".png", Comparator::make<ThresholdImageComparator>(
-            AbsThreshold(1.5 / 255.0), Percent(0.01)));
+    = Comparator::registerForAllExtensions<ThresholdCompareStrategy>();
